a4af6e151e964471e63ea20c5c74092077e69a6c
admin <admin@example.com>
2019-03-18 08:29:56 -0700

    Initial Commit

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from sqlalchemy import create_engine
-
-#create mysql user 'app' and grant read only on project db
-db_engine = create_engine('mysql+pymysql://app:password@localhost:3306/project')
-
-app = Flask(__name__)
-from app import views
